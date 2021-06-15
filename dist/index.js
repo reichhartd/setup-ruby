a8bc006d32776d66c9aaab9d17229066e1621b6b
Daniel Reichhart <daniel.reichhart@gmx.net>
2021-06-15 08:34:15 +0200

    Builded new version

--- dist/index.js
+++ dist/index.js
@@ -39853,11 +39853,11 @@ function shouldUseToolCache(engine, version) {
 function getPlatformToolCache(platform) {
   // Hardcode paths rather than using $RUNNER_TOOL_CACHE because the prebuilt Rubies cannot be moved anyway
   if (platform.startsWith('ubuntu-')) {
-    return `${__dirname}/hostedtoolcache`
+    return path.join(__dirname, '..', '..', '..', '..', '..', '_tool')
   } else if (platform.startsWith('macos-')) {
-    return `${__dirname}/hostedtoolcache`
+    return path.join(__dirname, '..', '..', '..', '..', '..', '_tool')
   } else if (platform.startsWith('windows-')) {
-    return `${__dirname}/hostedtoolcache`
+    return path.join(__dirname, '..', '..', '..', '..', '..', '_tool')
   } else {
     throw new Error('Unknown platform')
   }
